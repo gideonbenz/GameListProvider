c064b0ae0169d5877b28a96768b1a969b059b415
Gideon Benz <gideonbenz@gmail.com>
2019-11-02 10:47:23 +0700

    Initial Commit

---
+++ GideonBenzGameList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GideonBenzGameList
+//
+//  Created by Gideon Benz on 02/11/19.
+//  Copyright © 2019 gideonbenz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
